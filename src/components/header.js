51ef2d79f6e73914d199af19cddf017b3366e967
Mistry, Aneesh <aneesh.mistry@accenture.com>
2020-04-07 17:29:18 +0100

    updated first blog

--- src/components/header.js
+++ src/components/header.js
@@ -1,55 +1,44 @@
 import PropTypes from "prop-types"
 import React from "react"
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap'
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap"
 import yourSVG from "../images/logo_icon.svg"
 
-
-
 class Header extends React.Component {
-
   render() {
     return (
-
-      <Navbar fixed="top" light expand="md" >
-
+      <Navbar fixed="top" light expand="md">
         <div className="container" navbar>
-          <NavItem >
-            <a  href="/"
-                rel="noopener noreferrer">
+          <NavItem>
+            <a href="/" rel="noopener noreferrer">
               <i className="fas fa-home fa-2x"></i>
-              
             </a>
-            
-          </NavItem>                
+          </NavItem>
 
-          <div className="ml-auto" >
+          <div className="ml-auto">
             <Nav>
               <NavItem>
                 <NavLink href="/about">
                   <strong>
-                    <font size="+1">
-                      About
-                    </font>
+                    <font size="+1">About</font>
                   </strong>
                 </NavLink>
               </NavItem>
-              <NavItem >
-                <a href="https://www.hackerrank.com/4neesh"
+              <NavItem>
+                <a
+                  href="https://www.hackerrank.com/4neesh"
                   target="_blank"
-                  rel="noopener noreferrer">
+                  style={{ paddingTop: "10px" }}
+                  rel="noopener noreferrer"
+                >
                   <i className="fab fa-hackerrank fa-2x" />
                 </a>
               </NavItem>
               <NavItem>
-                <a href="https://www.linkedin.com/in/aneeshmistry/"
+                <a
+                  href="https://www.linkedin.com/in/aneeshmistry/"
                   target="_blank"
-                  rel="noopener noreferrer">
+                  rel="noopener noreferrer"
+                >
                   <i className="fab fa-linkedin fa-2x" />
                 </a>
               </NavItem>
@@ -61,20 +50,19 @@ class Header extends React.Component {
                 </a>
               </NavItem> */}
               <NavItem>
-                <a href="https://www.github.com/4neesh"
+                <a
+                  href="https://www.github.com/4neesh"
                   target="_blank"
-                  rel="noopener noreferrer">
+                  rel="noopener noreferrer"
+                >
                   <i className="fab fa-github fa-2x" />
                 </a>
               </NavItem>
             </Nav>
           </div>
         </div>
-      </Navbar >
-
-    
-
-    );
+      </Navbar>
+    )
   }
 }
 
