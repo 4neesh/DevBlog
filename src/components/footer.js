a08f95a259ca3755a3d58c2df9e19f0e15ec852b
Aneesh <aneesh@sky.com>
2020-10-07 14:40:11 +0100

    privacy policy and google analytics

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,8 @@ import React from "react"
 const Footer = () => (
 
     <div className="site-footer">
-        © {new Date().getFullYear()}, Aneesh Mistry
+        © {new Date().getFullYear()}, Aneesh Mistry | 
+        <a href="/privacy-policy"> Privacy Policy</a>
     </div>
 
 )
