6c0a370a4cdb6ecf4883b6441edf09dcc8c12a48
Aneesh <aneesh@sky.com>
2020-12-22 13:59:33 +0000

    cleaned pagination links for consistent view

--- src/components/PaginationLinks.js
+++ src/components/PaginationLinks.js
@@ -7,6 +7,9 @@ const PaginationLinks = ({ currentPage, numberOfPages }) => {
   const previousPage =
     currentPage - 1 === 1 ? '/blogs' : '/page/' + (currentPage - 1).toString()
   const nextPage = '/page/' + (currentPage + 1).toString()
+  const finalPage = '/page/' + numberOfPages
+  const firstPage = '/blogs'
+  const middleNumber = Math.round(numberOfPages /2)
   console.log("current: " + currentPage)
   console.log("previous: " + previousPage)
   console.log("nextPage: " + nextPage)
@@ -15,12 +18,24 @@ const PaginationLinks = ({ currentPage, numberOfPages }) => {
     <Pagination >
 
     
-    {/* make the fist button disabled if we are on the first page */}
+    {/* make the previous and first page button disabled if we are on the first page */}
+
+    {isFirst ? (
+        <PaginationItem disabled >
+          <PaginationLink  style={{color:"black"}} first  />
+        </PaginationItem>
+        
+      ) : (
+        <PaginationItem >
+          <PaginationLink style={{color:"black"}} first href={firstPage}/>
+        </PaginationItem>
+      )}
 
       {isFirst ? (
         <PaginationItem disabled >
           <PaginationLink  style={{color:"black"}} previous  />
         </PaginationItem>
+        
       ) : (
         <PaginationItem >
           <PaginationLink style={{color:"black"}} previous href={previousPage} />
@@ -28,35 +43,175 @@ const PaginationLinks = ({ currentPage, numberOfPages }) => {
       )}
   
 
-      {/* populate the middle numbers for pagination and colour active if selected */}
+
+
+
+      {/* populate the first 3 numbers for pagination and colour active if selected */}
       
-      {Array.from({ length: numberOfPages }, (_, i) =>
-        currentPage === i + 1 ? (
-          <PaginationItem  active key={`page-number${i + 1}`}>
-            <PaginationLink className="paginationLinks" href={`/${i === 0 ? 'blogs' : 'page/' + (i + 1)}`}>
-              {i + 1}
+      {isLast ? (
+
+<PaginationItem >
+            <PaginationLink style={{color:"black"}} href={`/${'blogs'}`}>
+              1  
             </PaginationLink>
           </PaginationItem>
-        ) 
-        : (
-          <PaginationItem key={`page-number${i + 1}`}>
-            <PaginationLink style={{color:"black"}} href={`/${i === 0 ? 'blogs' : 'page/' + (i + 1)}`}>
-              {i + 1}
+
+      )
+        :
+        currentPage + 2 === numberOfPages ? (
+          <PaginationItem  >
+            <PaginationLink style={{color:"black"}} href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage - 1)}`}>
+              {currentPage -1 }
             </PaginationLink>
           </PaginationItem>
         )
-      )}
+        :
+        currentPage + 1 === numberOfPages ? (
+          <PaginationItem  key={`page-number${currentPage + 1}`}>
+            <PaginationLink style={{color:"black"}} href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage - 2)}`}>
+              {currentPage -2 }
+            </PaginationLink>
+          </PaginationItem>
+        )
+        :
+        (
 
+<PaginationItem  active key={`page-number${currentPage + 1}`}>
+            <PaginationLink className="paginationLinks" href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage + 1)}`}>
+              {currentPage}
+            </PaginationLink>
+          </PaginationItem>
+
+        )
     
-    {/* make the next button disabled if on the final page*/}
+    }
+
+{isLast ? (
+
+<PaginationItem >
+            <PaginationLink style={{color:"black"}} href={`/${'page/2'}`}>
+              2  
+            </PaginationLink>
+          </PaginationItem>
+
+      )
+      
+        :
+        currentPage + 2 === numberOfPages ? (
+          <PaginationItem  active key={`page-number${currentPage + 1}`}>
+            <PaginationLink className="paginationLinks" href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage)}`}>
+              {currentPage }
+            </PaginationLink>
+          </PaginationItem>
+        )
+        :
+        currentPage + 1 === numberOfPages ? (
+          <PaginationItem  key={`page-number${currentPage + 1}`}>
+            <PaginationLink  style={{color:"black"}}  href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage -1 )}`}>
+              {currentPage -1 }
+            </PaginationLink>
+          </PaginationItem>
+        )
+       
+        :
+        
+        (
+
+<PaginationItem>
+            <PaginationLink style={{color:"black"}} href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage + 1)}`}>
+              {currentPage + 1}
+            </PaginationLink>
+          </PaginationItem>
+
+        )
+    
+    }
+
+{isLast ? (
+
+<PaginationItem >
+            <PaginationLink style={{color:"black"}} href={`/${'page/2'}`}>
+              3  
+            </PaginationLink>
+          </PaginationItem>
+
+      )
+        :
+        currentPage + 2 === numberOfPages ? (
+          <PaginationItem >
+            <PaginationLink style={{color:"black"}} href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage + 1)}`}>
+              {currentPage + 1 }
+            </PaginationLink>
+          </PaginationItem>
+        )
+        :
+        currentPage + 1 === numberOfPages ? (
+          <PaginationItem  active key={`page-number${currentPage + 1}`}>
+            <PaginationLink className="paginationLinks" href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage)}`}>
+              {currentPage }
+            </PaginationLink>
+          </PaginationItem>
+        )
+        :
+        
+        currentPage + 1 === numberOfPages ? (
+      null
+        )
+        :
+        (
+
+<PaginationItem>
+            <PaginationLink style={{color:"black"}} href={`/${currentPage === 0 ? 'blogs' : 'page/' + (currentPage + 2)}`}>
+              {currentPage + 2}
+            </PaginationLink>
+          </PaginationItem>
+
+        )
+    
+    }
+     
+
+      {/* create the ellipsis and the final page pagination */}
+
+      <PaginationItem disabled >
+            <PaginationLink style={{color:"black"}}  >
+              ...
+            </PaginationLink>
+      </PaginationItem>
+
+
+    {isLast ? (
+      <PaginationItem active >
+            <PaginationLink className="paginationLinks" href={finalPage} >
+              {numberOfPages}
+            </PaginationLink>
+      </PaginationItem>
+    ) : (
+      <PaginationItem >
+            <PaginationLink href={finalPage} style={{color:"black"}}  >
+              {numberOfPages}
+            </PaginationLink>
+      </PaginationItem>
+    )}
+
+    {/* make the next and last page button disabled if on the final page*/}
 
       {isLast ? (
         <PaginationItem disabled>
-          <PaginationLink style={{color:"black"}} next href={nextPage} />
+          <PaginationLink style={{color:"black"}} next  />
         </PaginationItem>
       ) : (
         <PaginationItem>
           <PaginationLink style={{color:"black"}} next href={nextPage} />
+        </PaginationItem>
+      )}
+      {isLast ? (
+        <PaginationItem disabled>
+          <PaginationLink style={{color:"black"}} last  />
+        </PaginationItem>
+      ) : (
+        <PaginationItem>
+          <PaginationLink style={{color:"black"}} last  href={finalPage}/>
         </PaginationItem>
       )}
     </Pagination>
